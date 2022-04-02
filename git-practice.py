9a9efe0ab9d9cc094d2295aed337e814e6c713e8
wolfwood0627 <102123065+wolfwood0627@users.noreply.github.com>
2022-04-02 10:27:41 +0900

    Update git-practice.py
    フィボナッチ数列のうち奇数のみを表示させるようにコードを書き換えました

--- git-practice.py
+++ git-practice.py
@@ -1,6 +1,11 @@
 # 100以下のフィボナッチ数列を表示
 a = 0
 b = 1
-while b < 100:
-    print(b)
+while b < 300:
     a, b = b, a+b
+    # 偶数の場合表示させない
+    if b % 2 == 0:
+        continue
+    print(b)
+    
+    
