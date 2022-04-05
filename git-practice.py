4cd4fe11de94fbbe52f0a7bf2d0581b8c44bca5e
wolfwood0627 <102123065+wolfwood0627@users.noreply.github.com>
2022-04-05 13:37:54 +0900

    Update git-practice.py
    偶数が表示されていたので、表示されないように変更
    でもこれフィボナッチ数列になってる気がしない。そもそも３が出力されない
    助けてプリーズ
    mainからコードを持ってくる方法がわかりませんでした

--- git-practice.py
+++ git-practice.py
@@ -1,11 +1,12 @@
-# 100以下のフィボナッチ数列を表示
+# 300以下のフィボナッチ数列を奇数のみ表示
 a = 0
 b = 1
-while b < 300:
+
+while a <= 300:
     a, b = b, a+b
-    # 偶数の場合表示させない
-    if b % 2 == 0:
-        continue
-    print(b)
+    if a % 2 == 0:
+        countinue
+    print(a)
+    a, b = b, a+b
     
     
